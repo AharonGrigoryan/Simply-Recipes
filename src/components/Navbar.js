1e8dd7e58a68911fa15930a7ada8ce5c95933a2a
Aharon <grigoryanaharon434@gmail.com>
2023-01-08 23:08:28 +0400

    added design and page layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,44 @@
 import { Link } from "gatsby";
 import React from "react";
+import { TfiAlignCenter } from "react-icons/tfi";
+import logo from "../assets/images/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/tags">Tags</Link>
-      <Link to="/resipes">Resipes</Link>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn">
+            <TfiAlignCenter />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            about
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,39 @@ import Layout from "../components/Layout";
 const Contact = () => {
   return (
     <Layout>
-      <h1>Hello Contact page</h1>
+      <section className="contact-page">
+        <article className="contact-info">
+          <h3>Want To Get In Touch?</h3>
+          <p>
+            Four dollar toast biodiesel plaid salvia actually pickled banjo
+            bespoke mlkshk intelligentsia edison bulb synth.
+          </p>
+          <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
+          <p>
+            Hashtag swag health goth air plant, raclette listicle fingerstache
+            cold-pressed fanny pack bicycle rights cardigan poke.
+          </p>
+        </article>
+        <article>
+          <form className="form contact-from">
+            <div className="form-row">
+              <label htmlFor="name">Your Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="email">Your Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="message">Massage</label>
+              <textarea name="message" id="massage"></textarea>
+            </div>
+            <button type="submit" className="btn block">
+              Submit
+            </button>
+          </form>
+        </article>
+      </section>
     </Layout>
   );
 };
