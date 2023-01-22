8f60122a02c373261ad1278324b2794f42f98ae4
Aharon <grigoryanaharon434@gmail.com>
2023-01-22 22:50:14 +0400

    change favicon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,47 +10,49 @@ const Contact = ({
 }) => {
   return (
     <Layout>
-      <section className="contact-page">
-        <article className="contact-info">
-          <h3>Want To Get In Touch?</h3>
-          <p>
-            Four dollar toast biodiesel plaid salvia actually pickled banjo
-            bespoke mlkshk intelligentsia edison bulb synth.
-          </p>
-          <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
-          <p>
-            Hashtag swag health goth air plant, raclette listicle fingerstache
-            cold-pressed fanny pack bicycle rights cardigan poke.
-          </p>
-        </article>
-        <article>
-          <form
-            className="form contact-from"
-            action="https://formspree.io/f/xeqwonzy"
-            method="POST"
-          >
-            <div className="form-row">
-              <label htmlFor="name">Your Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="email">Your Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="message">Massage</label>
-              <textarea name="message" id="massage"></textarea>
-            </div>
-            <button type="submit" className="btn block">
-              Submit
-            </button>
-          </form>
-        </article>
-      </section>
-      <section className="featured-recipes">
-        <h5>Look at this Awesomesouce!</h5>
-        <RecipesList recipes={recipes} />
-      </section>
+      <main className="page">
+        <section className="contact-page">
+          <article>
+            <h3>Want To Get In Touch?</h3>
+            <p>
+              Four dollar toast biodiesel plaid salvia actually pickled banjo
+              bespoke mlkshk intelligentsia edison bulb synth.
+            </p>
+            <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
+            <p>
+              Hashtag swag health goth air plant, raclette listicle fingerstache
+              cold-pressed fanny pack bicycle rights cardigan poke.
+            </p>
+          </article>
+          <article>
+            <form
+              className="form contact-from"
+              action="https://formspree.io/f/xeqwonzy"
+              method="POST"
+            >
+              <div className="form-row">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Massage</label>
+                <textarea name="message" id="massage"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                Submit
+              </button>
+            </form>
+          </article>
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
+      </main>
     </Layout>
   );
 };
