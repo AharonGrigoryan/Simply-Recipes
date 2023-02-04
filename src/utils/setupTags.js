1db4efb463fe551171e012fecf0a2e9d260b304c
Aharon <grigoryanaharon434@gmail.com>
2023-02-04 21:30:14 +0400

    .

--- src/utils/setupTags.js
+++ src/utils/setupTags.js
@@ -20,3 +20,32 @@ const setupTags = (recipes) => {
 };
 
 export default setupTags;
+
+// import React from "react";
+// import { Helmet } from "react-helmet";
+// import { useStaticQuery, graphql } from "gatsby";
+
+// const query = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//         description
+//       }
+//     }
+//   }
+// `;
+
+// const Seo = ({ title, description }) => {
+//   const { site } = useStaticQuery(query);
+//   const metaDescription = description || site.siteMetadata.description;
+//   return (
+//     <Helmet
+//       htmlAttributes={{ lang: "en" }}
+//       title={`${title} | ${site.siteMetadata.title}`}
+//       meta={[{ name: "description", content: metaDescription }]}
+//     ></Helmet>
+//   );
+// };
+
+// export default Seo;
